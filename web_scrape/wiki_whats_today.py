1507078ff389588f731cbd35cc7253f367143189
sasasoni <sasasoni1119@gmail.com>
2020-10-20 16:35:33 +0900

    modified parse attr

--- web_scrape/wiki_whats_today.py
+++ web_scrape/wiki_whats_today.py
@@ -7,17 +7,18 @@ url = "https://ja.wikipedia.org/"
 response = requests.get(url)
 soup = BeautifulSoup(response.content, "html.parser")
 
-today = soup.find("div", attrs={"id": "mf-itn"})
+today = soup.find("div", attrs={"id": "on_this_day"})
 entries = today.find_all("li")
 
 today_list = []
 # enumerate()でリスト[a,b,c]にインデックス番号をつけて取り出す関数[[0, a],[1, b],[2,c]]みたいな
-for i, entry in enumerate(entries):
+# 第二引数で始まりの数字を指定できる
+for i, entry in enumerate(entries, 1):
     match = re.findall("(（.*?）)", entry.get_text())
     if match:
-        today_list.append([i+1, entry.get_text(), match[-1]])
+        today_list.append([i, entry.get_text(), int(match[-1][1:-2])])
     else:
-        today_list.append([i+1, entry.get_text()])
+        today_list.append([i, entry.get_text()])
 # with open("output.csv", "w", encoding="UTF-8") as file:
 #     writer = csv.writer(file, lineterminator="\n")
 #     writer.writerows(today_list)
